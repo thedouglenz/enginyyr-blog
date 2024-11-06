6aeba27b37e6f7b5b7d7dd0cbb9d46cfbf6698ca
Douglas Lenz <thedouglenz@gmail.com>
2024-11-06 12:50:23 -0500

    Remove `output` from astro config

--- astro.config.ts
+++ astro.config.ts
@@ -8,7 +8,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   site: SITE.website,
   integrations: [
     tailwind({
