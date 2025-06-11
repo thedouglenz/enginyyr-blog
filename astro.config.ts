349ad00cfcf770f8098a334417e7031b3b2b5473
Douglas Lenz <thedouglenz@gmail.com>
2025-06-11 09:41:14 -0400

    Mermaid client-side rendering fixes

--- astro.config.ts
+++ astro.config.ts
@@ -5,17 +5,26 @@ import remarkCollapse from "remark-collapse";
 import remarkToc from "remark-toc";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import rehypeMermaid from 'rehype-mermaid';
-import addMermaidClass from './add-mermaid-classname';
+import mdxMermaid from 'mdx-mermaid';
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  }), react(), sitemap(), mdx()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+    sitemap(),
+    mdx({
+      remarkPlugins: [
+        // @ts-ignore
+        [mdxMermaid, { output: 'svg' }]
+      ]
+    }),
+  ],
   markdown: {
     remarkPlugins: [
       remarkToc,
@@ -25,13 +34,6 @@ export default defineConfig({
           test: "Table of contents",
         },
       ],
-    ],
-    rehypePlugins: [
-      addMermaidClass,
-      [rehypeMermaid, {
-        strategy: 'img-svg',
-        dark: true
-      }],
     ],
     shikiConfig: {
       // For more themes, visit https://shiki.style/themes
